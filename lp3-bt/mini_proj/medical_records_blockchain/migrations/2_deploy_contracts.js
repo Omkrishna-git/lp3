6253c506e838c784a5b4b43b10658f4a4da06ec2
Omkrishna-git <omdpatil07@gmail.com>
2025-11-09 22:13:32 +0530

    Bt start

--- lp3-bt/mini_proj/medical_records_blockchain/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const MedicalRecord = artifacts.require("MedicalRecord");
-
-module.exports = function(deployer) {
-  // Deploy with a sample encrypted hash
-  const initialHash = "QmSampleEncryptedMedicalDataHash123456789";
-  deployer.deploy(MedicalRecord, initialHash);
-};
